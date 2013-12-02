0ba1aafb1a3603532e187dcd763a2b6746880f3c
Pedro Veronezi <pedroveronezi@gmail.com>
2013-12-02 21:34:59 -0200

    Capitulo 3 - pag 78

--- Boa Viagem/src/main/java/br/com/casadocodigo/boaviagem/DashboardActivity.java
+++ Boa Viagem/src/main/java/br/com/casadocodigo/boaviagem/DashboardActivity.java
@@ -1,12 +1,9 @@
 package br.com.casadocodigo.boaviagem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import br.com.casadocodigo.boaviagem.R;
 
 /**
  * Created by veronezi on 01/12/13.
@@ -14,12 +11,17 @@ import br.com.casadocodigo.boaviagem.R;
 public class DashboardActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard_activity);
+        setContentView(R.layout.activity_dashboard);
     }
 
     public void selecionarOpcao(View view) {
-        TextView textView = (TextView) view;
-        String opcao = "Opção: " + textView.getText().toString();
-        Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+        switch (view.getId()) {
+            case R.id.nova_viagem:
+                startActivity(new Intent(this, ViagemActivity.class));
+                break;
+
+        }
+
+
     }
 }
---
+++ Boa Viagem/src/main/java/br/com/casadocodigo/boaviagem/ViagemActivity.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.boaviagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by veronezi on 02/12/13.
+ */
+public class ViagemActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viage);
+    }
+
+    public void salvarViagem(View view) {
+    }
+
+    public void selecionarData(View view) {
+    }
+}
