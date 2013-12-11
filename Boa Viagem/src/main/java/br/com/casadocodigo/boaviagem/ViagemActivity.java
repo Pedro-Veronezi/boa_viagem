c2400bcc31fa6e351b24693e074318562209620e
Pedro Veronezi <pedroveronezi@gmail.com>
2013-12-11 07:34:15 -0200

    Correção de Layout

--- Boa Viagem/src/main/java/br/com/casadocodigo/boaviagem/ViagemActivity.java
+++ Boa Viagem/src/main/java/br/com/casadocodigo/boaviagem/ViagemActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 public class ViagemActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_viage);
+        setContentView(R.layout.activity_viagem);
     }
 
     public void salvarViagem(View view) {
