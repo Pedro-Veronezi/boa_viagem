241e4091b42186c7bf845e162260b5abd21198be
Pedro Veronezi <pedroveronezi@gmail.com>
2014-01-13 21:58:03 -0200

    Capitulo 3 - pag 95
    Criado layout para a lista da gastos

--- Boa Viagem/src/main/java/br/com/casadocodigo/boaviagem/ViagemListActivity.java
+++ Boa Viagem/src/main/java/br/com/casadocodigo/boaviagem/ViagemListActivity.java
@@ -7,31 +7,56 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by pcampos on 07/01/14.
  */
 public class ViagemListActivity extends ListActivity implements AdapterView.OnItemClickListener{
+    private List<Map<String, Object>> viagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listarViagens()));
-        ListView listView = getListView();
-        listView.setOnItemClickListener(this);
+        String[] de = {"imagem", "destino", "data", "total"};
+        int[] para = {R.id.tipoViagem, R.id.destino, R.id.data, R.id.valor};
+
+        SimpleAdapter adapter;
+        adapter = new SimpleAdapter(this, listarViagens(), R.layout.lista_viagem, de, para);
+
+        setListAdapter(adapter);
+
+        getListView().setOnItemClickListener(this);
     }
 
-    private List<String> listarViagens() {
-        return Arrays.asList("São Paulo","Bonito","Maceió");
+    private List<Map<String, Object>> listarViagens() {
+        viagens = new ArrayList<Map<String,Object>>();
+        Map<String, Object> item = new HashMap<String, Object>();
+        item.put("imagem", R.drawable.negocios);
+        item.put("destino", "São Paulo");
+        item.put("data","02/02/2012 a 04/02/2012");
+        item.put("total","Gasto total R$ 314,98");
+        viagens.add(item);
+        item = new HashMap<String, Object>();
+        item.put("imagem", R.drawable.lazer);
+        item.put("destino", "Maceió");
+        item.put("data","14/05/2012 a 22/05/2012");
+        item.put("total","Gasto total R$ 25834,67");
+        viagens.add(item);
+        return viagens;
     }
 
-    @Override
+
+        @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         TextView textView = (TextView) view;
         String mensagem = "Viagem selecionada: " + textView.getText();
